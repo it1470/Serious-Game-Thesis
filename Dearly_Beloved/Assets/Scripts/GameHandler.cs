677a5b755ee79903dd27bbe658b95f8e2f219cae
it1470 <it1470@uom.edu.gr>
2021-10-05 15:03:02 +0300

    Created title menu.
    Created title menu with music.
    
    Sources used{
    
    -Brackeys{
    `START MENU in Unity
    https://youtu.be/zc8ac_qUXQY
    }
    -Hooson{
    `VOLUME SLIDER
    https://www.youtube.com/watch?v=yWCHaTwVblk
    }
    
    -Background image
    https://pressstart.vip/assets
    
    -Title font
    https://www.dafont.com/sketch-3d.font?af=on&text=Dearly+Beloved
    
    -Title Flourish
    https://www.cleanpng.com/png-picture-frames-room-clip-art-vector-decorative-bow-2293373/
    }

---
+++ Dearly_Beloved/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("its alll fine");
+
+        Debug.Log("why is it so hard");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+    }
+}
